2f8ceb66005826ebf2df634bdde893abe2cc21b1
moguifeng <moguifeng@kobolink.com>
2020-04-03 11:21:46 +0800

    run.log

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,3 +6,4 @@ cd /data/services/recsys_learning
 pyenv shell 3.6.8
 python modules/ncf/DataPrepare.py
 python modules/ncf/NCF.py --path=/data/services/recsys_learning/datas/info --dataset=info --epochs=16 --num_factors=16
+ossutil cp emb.txt oss://kobo-recsys/di/ncf/emb.txt
